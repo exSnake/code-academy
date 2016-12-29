5ea45d30e4b6aec925fb04525b1afba922cb35ac
exSnake <giaqui@gmail.com>
2016-12-29 22:24:28 +0100

    Changed Index.html to php

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang=\"en\">
+
+<?php
+    include "head.php";
+    include "header.php";
+?>
+<body>
+<div class="table-title">
+    <h3>PROVVISIONALE</h3>
+</div>
+<table class="table-fill">
+    <thead>
+    <tr>
+        <th class="text-left">Esercente</th>
+        <th class="text-left">Cassetto</th>
+        <th class="text-left">Casse</th>
+        <th class="text-left">Cassa</th>
+        <th class="text-left">Chiusura</th>
+    </tr>
+    </thead>
+    <tbody class="table-hover">
+    <tr>
+        <td class="text-left">Autolavaggio</td>
+        <td class="text-left">3560</td>
+        <td class="text-left">1600</td>
+        <td class="text-left">25/02/2016</td>
+        <td class="text-left">20/01/2016</td>
+    </tr>
+    <tr>
+        <td class="text-left">Bar Stella</td>
+        <td class="text-left">6650</td>
+        <td class="text-left">4000</td>
+        <td class="text-left">11/11/2016</td>
+        <td class="text-left">23/03/2016</td>
+    </tr>
+    <tr>
+        <td class="text-left">Metelliana</td>
+        <td class="text-left">300</td>
+        <td class="text-left">0</td>
+        <td class="text-left">27/12/2016</td>
+        <td class="text-left">22/12/2016</td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
